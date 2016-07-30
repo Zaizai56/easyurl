e3855e13be317c2d965cc653f5005e8ba257cdea
Isaie Courilleau <icourilleau@hotmail.fr>
2016-07-30 08:31:55 +0000

    deploy

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 var mongo = require('mongodb').MongoClient;
 var tools = require('./app/api/tools');
 
-var ip = process.env.IP;
+console.log("app running");
+
 var port = process.env.PORT || 8080; // set our port
 
 app.use(express.static(__dirname + "/app/public"));
